620af6dc20e6f4e4142b37b02087077e99c60fb5
Shayane <shayaneteixeira56@gmail.com>
2022-05-27 15:41:58 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
-const navMenu = document.getElementById('nav-menu'),
-  navToggle = document.getElementById('nav-toggle'),
-  navClose = document.getElementById('nav-close');
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
 if (navToggle) {
   navToggle.addEventListener('click', () => {
@@ -55,31 +55,3 @@ function backToTop() {
     backToTopButton.classList.remove('show')
   }
 }
-
-
-const sections = document.querySelectorAll('section[id]');
-function scrollActive() {
-  const scrollY = window.pageYOffset;
-  sections.forEach((current) => {
-    const sectionHeight = current.offsetHeight,
-      sectionTop = current.offsetTop - 58,
-      sectionId = current.getAttribute('id');
-    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
-      document
-        .querySelector('.nav_menu a[href*=' + sectionId + ']')
-        .classList.add('active-link');
-    } else {
-      document
-        .querySelector('.nav_menu a[href*=' + sectionId + ']')
-        .classList.remove('active-link');
-    }
-  });
-}
-window.addEventListener('scroll', scrollActive);
-
-function scrollUp() {
-  const scrollUp = document.getElementById('scroll-up');
-  if (this.scrollY >= 350) scrollUp.classList.add('show-scroll');
-  else scrollUp.classList.remove('show-scroll');
-}
-window.addEventListener('scroll', scrollUp);
